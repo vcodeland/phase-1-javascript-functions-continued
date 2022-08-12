961cdb0897fa46be8d9b31fc2fcbe46f7ae563db
Vernitra Jones <vernitrasmedia@gmail.com>
2022-08-11 19:30:37 -0500

    commit tree 6942e42fb9e0ef16b9557774e1ac07de0e585e10

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`)
+  }
+  
+console.log(saturdayFun());
+
+mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+console.log(mondayWork());
